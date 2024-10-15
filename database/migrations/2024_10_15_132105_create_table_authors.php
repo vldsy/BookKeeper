aa09e1c562390a55cf862ac3d4024b8d67d588d9
vldsy <vldsyata@gmail.com>
2024-10-15 15:51:06 +0200

    corrected migrations

--- database/migrations/2024_10_15_132105_create_table_authors.php
+++ database/migrations/2024_10_15_132105_create_table_authors.php
@@ -24,6 +24,6 @@ return new class () extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('table_authors');
+        Schema::dropIfExists('authors');
     }
 };
