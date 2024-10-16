58bb670258fc7869780112e636cc128c8c0f2f70
XoDeR <vldsyata@gmail.com>
2024-10-16 22:15:44 +0200

    displaying all books

--- resources/js/Pages/Books/Index.vue
+++ resources/js/Pages/Books/Index.vue
@@ -1,8 +1,15 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import Book from '@/Components/Book.vue';
+import { useForm, Head } from '@inertiajs/vue3';
+
+const props = defineProps(['books']);
+
+import { ref, computed, onMounted } from 'vue'
+
 </script>
 
 <template>
-
+    <!-- display json for debug purposes -->
+    <!-- replace 2 with '\t' to do tab indentation  -->
+    <pre>{{ JSON.stringify(props.books, null, 2) }}</pre>
 </template>
