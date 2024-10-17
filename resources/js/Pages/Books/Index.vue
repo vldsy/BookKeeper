a30a1df35b32fd9292261923a71ecd8adc96660b
XoDeR <vldsyata@gmail.com>
2024-10-17 21:40:22 +0200

    added inner page layout to books

--- resources/js/Pages/Books/Index.vue
+++ resources/js/Pages/Books/Index.vue
@@ -60,14 +60,45 @@ const rows = computed(() => {
     <Head title="Books" />
 
     <AuthenticatedLayout>
+        <div class="mx-auto w-full px-4 md:px-10">
+            <div class="mt-4 flex flex-wrap">
+                <div class="mb-12 w-full px-4 xl:mb-0 xl:w-8/12">
+                    <div class="relative mb-6 flex w-full min-w-0 flex-col break-words rounded bg-white shadow-lg">
+                        <div class="mb-0 rounded-t border-0 px-4 py-3">
+                            <div class="flex flex-wrap items-center">
+                                <div class="relative w-full max-w-full flex-1 flex-grow px-4">
+                                    <h3 class="text-blueGray-700 text-base font-semibold">Books</h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="block w-full overflow-x-auto">
+                            <div class="overflow-x-auto">
+                                <DataTableBooks :rows="rows" :columns="columns" />
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="w-full px-4 xl:w-4/12">
+                    <div class="relative mb-6 flex w-full min-w-0 flex-col break-words rounded bg-white shadow-lg">
+                        <div class="mb-0 rounded-t border-0 px-4 py-3">
+                            <div class="flex flex-wrap items-center">
+                                <div class="relative w-full max-w-full flex-1 flex-grow px-4">
+                                    <h3 class="text-blueGray-700 text-base font-semibold">Borrowed books</h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="block w-full overflow-x-auto"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
         <!-- display json for debug purposes -->
         <!-- replace 2 with '\t' to do tab indentation  -->
         <!-- <pre>{{ JSON.stringify(props.books, null, 2) }}</pre> -->
 
-        <div class="overflow-x-auto">
-            <DataTableBooks :rows="rows" :columns="columns" />
-        </div>
+
 
     </AuthenticatedLayout>
 
